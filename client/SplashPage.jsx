46aed455e76c1552f05fd75ccf58b64be91fcbb1
Hudson McLane <hud.mclane@gmail.com>
2023-11-11 12:25:50 -0500

    added additional logic for buttons

--- client/SplashPage.jsx
+++ client/SplashPage.jsx
@@ -4,4 +4,6 @@
     //list is rendered as each existingForm, put into ul element as li
     //next to each existing form, is hyperlink?/button to route to viewOneForm
     
+
 // contains button to create new form (new page)
+//sort by logic to change list of existing forms by corresponding tag
